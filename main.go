032f2a57c194da5fbea8f4d2dfbaadbf32062fe2
Jamie <JamieThomasAnderson@protonmail.com>
2026-08-15 10:43:33 +1000

    use golang.org/x/sys/unix instead of syscall

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ import (
 )
 
 func main() {
+
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "usage: tinyrun run <cmd> [args...]")
+		os.Exit(1)
+	}
+
 	switch os.Args[1] {
 	case "run":
 		parent()
@@ -24,15 +30,17 @@ func parent() {
 	self, err := os.Executable()
 	must(err)
 
-
 	cmd := exec.Command(
-		self,	
+		self,
 		append([]string{"child"},
 			os.Args[2:]...,
 		)...,
 	)
 
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+
+	// syscall.SysProcAttr stays: os/exec types this field as *syscall.SysProcAttr,
+	// so unix has no equivalent here. It's a struct type, not a deprecated call.
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: unix.CLONE_NEWUTS | unix.CLONE_NEWPID | unix.CLONE_NEWNS,
 	}
@@ -42,16 +50,18 @@ func parent() {
 
 func child() {
 
-	must(syscall.Sethostname([]byte("tinyrun")))
-	must(syscall.Chroot("./rootfs"))
+	must(unix.Sethostname([]byte("tinyrun")))
+	must(unix.Chroot("./rootfs"))
 	must(os.Chdir("/"))
-	must(syscall.Mount("proc", "/proc", "proc", 0, ""))
+
+	must(unix.Mount("", "/", "", unix.MS_REC|unix.MS_PRIVATE, ""))
+	must(unix.Mount("proc", "/proc", "proc", 0, ""))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
 	must(cmd.Run())
 
-	must(syscall.Unmount("/proc", 0))
+	must(unix.Unmount("/proc", 0))
 }
 
 func must(err error) {
